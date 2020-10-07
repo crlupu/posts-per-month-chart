d068768930c579865fcfeeac5648c5146ac2fcf2
crlupu <clupu140@gmail.com>
2020-10-07 22:07:48 +0300

    add chart to display posts data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 import './App.css';
+import { scaleBand, scaleLinear } from '@visx/scale';
+import { Group } from '@visx/group';
+import { Bar } from '@visx/shape';
 
 const GET_POSTS = gql`
 {
@@ -9,6 +12,9 @@ const GET_POSTS = gql`
   }
 }
 `
+
+const getMonth = d => d.label;
+const getMonthPosts = d => d.value;
 
 function App() {
   const { data, loading, error } = useQuery(GET_POSTS);
@@ -29,9 +35,49 @@ function App() {
     }
   });
 
-  console.log(postsByMonth);
+  const width = 850;
+  const height = 500;
+
+  const xMax = width - 100;
+  const yMax = height - 120;
+
+  const xScale = scaleBand({
+    range: [0, xMax],
+    domain: postsByMonth.map(getMonth),
+    padding: 0.5
+  });
+
+  const yScale = scaleLinear({
+    range: [yMax, 0],
+    domain: [0, Math.max(...postsByMonth.map(getMonthPosts))]
+  });
+
   return (
-    <div></div>
+    <div>
+      <svg width={width} height={height}>
+        <rect width={width} height={height} fill='lightblue' rx={14} />
+        <Group top={60} left={80}>
+          {postsByMonth.map(p => {
+            const month = getMonth(p);
+            const barWidth = xScale.bandwidth();
+            const barHeight = yMax - yScale(getMonthPosts(p));
+            const barX = xScale(month);
+            const barY = yMax - barHeight;
+
+            return (
+              <Bar
+                key={`bar-${month}`}
+                x={barX}
+                y={barY}
+                width={barWidth}
+                height={barHeight}
+                fill="red"
+              />
+            )
+          })};
+        </Group>
+      </svg>
+    </div>
   )
 }
 
