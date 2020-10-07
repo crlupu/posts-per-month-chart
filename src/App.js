e663835d91cd252f38f60f890d862d71e864908e
crlupu <clupu140@gmail.com>
2020-10-07 19:44:50 +0300

    fetch posts data from server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const GET_POSTS = gql`
+{
+  products: allPosts(count: 10000) {
+    createdAt
+  }
+}
+`
+
 function App() {
+  const { data, loading, error } = useQuery(GET_POSTS);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div></div>
+  )
 }
 
 export default App;
